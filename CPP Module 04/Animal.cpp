0abe3ef5d89e07bdc177b1c4c6e2cc22a36cb06e
Kheireddine-Anas <kheireddine.anas@gmail.com>
2024-11-24 21:37:09 +0100

    CPP 04

---
+++ CPP Module 04/Animal.cpp
@@ -0,0 +1,32 @@
+#include "Animal.hpp"
+
+
+Animal::Animal() {
+	std::cout << "Constructor Called For Animal Class" << std::endl;	
+}
+
+Animal::~Animal() {
+	std::cout << "Destructor Called For Animal Class" << std::endl;	
+	
+}
+
+Animal::Animal( const Animal& other ) {
+	std::cout << "Copy Constructor Called For Animal Class" << std::endl;	
+	
+}
+
+Animal& Animal::operator=( const Animal& other ) {
+	if (this != &other) {
+		this->type = other.type;
+	}
+	std::cout << "Assignment operator Called For Animal Class" << std::endl;
+	return *this;
+}
+
+void Animal::makeSound() {
+	std::cout << "Animals Sound" << std::endl;
+}
+
+std::string Animal::getType() {
+	return this->type;
+}
